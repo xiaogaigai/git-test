e32989afa919c057b87e546cbdbdbde4738dc59b
csh <599557617@qq.com>
2019-06-08 20:44:56 +0800

    修改工程至本地仓库

---
+++ src/com/itheima/GitDemo01.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class GitDemo01 {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈~");
+    }
+    public void Test(){
+        int i = 0;
+        System.out.println(i);
+    }
+}
