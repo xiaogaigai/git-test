0b42ea510d13151ee3fc995d766106925bb49fd4
csh <599557617@qq.com>
2019-06-08 21:38:24 +0800

    修改远程仓库工程代码

--- src/com/itheima/GitDemo01.java
+++ src/com/itheima/GitDemo01.java
@@ -3,9 +3,11 @@ package com.itheima;
 public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈~");
+        //hello test
     }
     public void Test(){
         int i = 0;
+        System.out.println("i输出为：");
         System.out.println(i);
     }
 }
